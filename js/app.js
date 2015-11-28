169bc9b17ea042d1d9108d31dd6a73ddfe095a69
Woosuk Seo <seow@uw.edu>
2015-11-27 23:03:15 -0800

    Basic page outline is done. Partials are linked

--- js/app.js
+++ js/app.js
@@ -1,1 +1,62 @@
 'use strict';
+
+angular.module('MainApp', ['ngSanitize', 'ui.router'])
+.config(function($stateProvider){
+	$stateProvider
+		.state('home', {
+			url: '/', //"root" directory
+			templateUrl: 'partials/home.html'
+		})
+
+		.state('about', {
+			url: '/about',
+			templateUrl: 'partials/about.html',
+			controller:'AboutCtrl'
+		})
+
+		.state('recruit', {
+			url: '/recruit',
+			templateUrl: 'partials/recruit.html',
+			controller: 'RecruitCtrl'
+		})
+
+		.state('gallery', {
+			url: '/gallery',
+			templateUrl: 'partials/gallery.html',
+			controller: 'GalleryCtrl'
+		})
+
+		.state('philanthropy', {
+			url: '/philanthropy',
+			templateUrl: 'partials/philanthropy.html',
+			controller: 'PhilanthropyCtrl'
+		})
+
+		.state('contact', {
+			url: '/contact',
+			templateUrl: 'partials/contact.html'
+		})
+
+})
+
+//Controller for About page
+.controller('AboutCtrl', ['$scope', '$http', function($scope, $http) {
+
+}])
+
+//Controller for Recruit page
+.controller('RecruitCtrl', ['$scope', '$http', function($scope, $http) {
+
+}])
+
+//Controller for Gallery page
+.controller('GalleryCtrl', ['$scope', '$http', function($scope, $http) {
+
+}])
+
+//Controller for Philanthropy page
+.controller('PhilanthropyCtrl', ['$scope', '$http', function($scope, $http) {
+
+}])
+
+
